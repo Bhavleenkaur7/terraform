4ba041667dec09df7fb8c506463c36b07c737216
ttn-aditya <aditya.kumar@tothenew.com>
2019-06-11 15:02:53 +0530

    main.tf updated

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 module "ec2" {
-  source        = "./ec2-stack"
+  source        = "./ec2-alb/ec2-stack"
   ami_id        = var.ami_id
   name          = var.name
   ec2_subnet_id = var.ec2_subnet_id
@@ -31,3 +31,20 @@ module "ec2" {
   #variables               = "${var.variables}"
 }
 
+locals {
+  name = "ecs-service"
+  environment = "dev"
+}
+module "ecs-service" {
+  name = "${local.name}"
+  source = "./ecs-service/modules/ecs-service"
+  alb_dns_name = "${var.alb_dns_name}"
+  alb_listener = "${var.alb_listener}"
+  alb_zone_id = "${var.alb_zone_id}"
+  cluster = "${var.cluster}"
+  container_definitions = "${var.container_definitions}"
+  iam_role = "${var.iam_role}"
+  rule_priority = "${var.rule_priority}"
+  vpc_id = "${var.vpc_id}"
+  zone_id = "${var.zone_id}"
+}
