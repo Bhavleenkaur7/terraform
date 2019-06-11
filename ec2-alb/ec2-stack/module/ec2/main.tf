b433ae6f39f32793c848fb8fed84de70d9fcb6fe
ttn-aditya <aditya.kumar@tothenew.com>
2019-06-11 12:45:48 +0530

    update bug fixex

--- ec2-alb/ec2-stack/module/ec2/main.tf
+++ ec2-alb/ec2-stack/module/ec2/main.tf
@@ -22,6 +22,6 @@ resource "aws_instance" "this" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "${var.key_name}"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDV2x63e68lnWSFDuEuSxpjsiYrwPBBVH7kwmM0q/Fm2/6Ydi6LlCPhZG/v2s3tHVnorrFSTGj4bOztdAWpwY6Tj/JmYskGnR03DftI1Q7wp2GVxEzDExE9bJMdJlX9anaxC0JUXA4eoX/bGItyAolLssQuuQzy2ggUS2KiFDD7YjibauQV+Vv7yvV8CHpJYmWVRpk/XRWRsE+p/NrncKoJLBkUBLBbSX0Di9eu0tjkGF0PvYs7/kGEPS7D98g2QVA/7KHhGZFAS+HW35GrTMLDo7DaQqyptyE9y0QIP2XTCcuB/uh9DzOCNOq4fj+PYJbyr6ZfX92PmJTsTa7u0KlXiYjoxIy5POSby98HaTBXoRqnlNepXgL1on2JIqlDJIYDfy+JAOw9bTvKiYgIo52tiikZU0ZR8KpianeRtBXZIrj0AntnxwnCwWBQLmXOkhX7tPQxRTDxxMt1A2KIZr5qpYzVFnkuezQfAMK6SPJr3EBxTDI+uhWuZ3c6WtAaqecCULXNcAvztRoZwnjsRNMNRbos6Xzf0luhktn5iiv4qgQtd1ylPNm/RDS6PmPtaZI7MYORiUs3vvg/CXa/1d8i4eYWA05OrnuaBgO+m61CFaxqLRwqgPTshKswP88Upew6Zb6g4ufmurL9ZbqiTSK3oICH+iZ0uUyUWsf0vsYZiw=="
+  public_key = "xxxxxxxxxxxxENTER YOUR SYSTEM PUBLIC KEY HERExxxxxxxxxxxxxxxxx"
 }
 
